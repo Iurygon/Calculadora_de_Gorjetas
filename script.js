a83bb3b2d22629073531d4ac32e6d4d85251dad9
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-06-08 23:59:37 -0300

    Adição de script

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Elementos de formulário */
+const form_Conta = document.getElementById('conta');
+const form_Pessoas = document.getElementById('pessoas');
+/* Elementos de escolha da porcentagem de gorjeta */
+const gorjeta_5 = document.getElementById('gorjeta_5');
+const gorjeta_10 = document.getElementById('gorjeta_10');
+const gorjeta_15 = document.getElementById('gorjeta_15');
