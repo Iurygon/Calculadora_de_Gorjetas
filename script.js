f1e2bd021a413678392a4a7c275f7e2a2e03a305
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-06-14 00:59:01 -0300

    Mudança de script e alterações em HTML e CSS

---
+++ script.js
@@ -0,0 +1,12 @@
+function receberValor(){
+//Recebe os valores dos inputs digitados pelo usuário//  
+    var valorConta = document.getElementById("conta").value;
+    var gorjetaOutro = document.getElementById("gorjeta_outro").value;
+    var numPessoas = document.getElementById("pessoas").value;
+//Recebe os valores dos inputs selecionados no campo de gorjeta//
+    var gorjeta_5 = document.getElementById("gorjeta_5").value;
+    var gorjeta_10 = document.getElementById("gorjeta_10").value;
+    var gorjeta_15 = document.getElementById("gorjeta_15").value;
+    var gorjeta_25 = document.getElementById("gorjeta_25").value;
+    var gorjeta_50 = document.getElementById("gorjeta_50").value;
+}
