31280b7d4f469016855161d0434ae311fd66358e
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-06-27 00:08:12 -0300

    Testes feitos em JS

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ function receberValorPessoas(){
 
 //Função para receber valor da gorjeta selecionado//
 function receberValorGorjeta(){
-    var gorjeta_5 = document.getElementById("gorjeta_5").value;
-    var gorjeta_10 = document.getElementById("gorjeta_10").value;
-    var gorjeta_15 = document.getElementById("gorjeta_15").value;
-    var gorjeta_25 = document.getElementById("gorjeta_25").value;
-    var gorjeta_50 = document.getElementById("gorjeta_50").value;
+    var valorGorjeta = document.forms['gorjeta'];
+    document.getElementById('tot_Gorjeta').innerHTML = valorGorjeta;
 }
