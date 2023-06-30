8863396f53eff5a456d9c7a816486d7e583a5763
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-06-29 23:57:20 -0300

    Mudança no código JS e HTML

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-//Funções para receber valores digitados pelo usuário//
+//Receber valor da conta//
 function receberValorConta(){  
     var valorConta = document.getElementById("conta").value;
 }
 
-function receberValorGorjeta_Outro(){
-    var gorjetaOutro = document.getElementById("gorjeta_outro").value;
-}
-
+//Receber valor do número de pessoas//
 function receberValorPessoas(){
     var numPessoas = document.getElementById("pessoas").value;
 }
 
-//Função para receber valor da gorjeta selecionado//
-function receberValorGorjeta(){
-    var valorGorjeta = document.querySelector('input[name="porcentagem_gorjeta"]:checked').value;
-    document.getElementById('tot_Gorjeta').innerHTML = valorGorjeta;
+//Receber valor da gorjeta selecionada//
+function receberValorGorjeta5(){
+    var valorGorjeta_5 = document.getElementById("gorjeta_5").value;
+}
+
+function receberValorGorjeta10(){
+    var valorGorjeta_10 = document.getElementById("gorjeta_10").value;
+}
+
+function receberValorGorjeta15(){
+    var valorGorjeta_15 = document.getElementById("gorjeta_15").value;
+}
+
+function receberValorGorjeta25(){
+    var valorGorjeta_25 = document.getElementById("gorjeta_25").value;
+}
+
+function receberValorGorjeta50(){
+    var valorGorjeta_50 = document.getElementById("gorjeta_50").value;
+}
+
+function receberValorGorjeta_Outro(){
+    var gorjetaOutro = document.getElementById("gorjeta_outro").value;
 }
