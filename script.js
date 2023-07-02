3094477d8ec08e588d7037101007d0f1c7576450
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-07-01 23:58:58 -0300

    Adição de comentários ao código JS

--- script.js
+++ script.js
@@ -9,26 +9,32 @@ function receberValorPessoas(){
 }
 
 //Receber valor da gorjeta selecionada//
+//Recebe o valor do input de 5%//
 function receberValorGorjeta5(){
     var valorGorjeta_5 = document.getElementById("gorjeta_5").value;
 }
 
+//Recebe o valor do input de 10%//
 function receberValorGorjeta10(){
     var valorGorjeta_10 = document.getElementById("gorjeta_10").value;
 }
 
+//Recebe o valor do input de 15%//
 function receberValorGorjeta15(){
     var valorGorjeta_15 = document.getElementById("gorjeta_15").value;
 }
 
+//Recebe o valor do input de 25%//
 function receberValorGorjeta25(){
     var valorGorjeta_25 = document.getElementById("gorjeta_25").value;
 }
 
+//Recebe o valor do input de 50%//
 function receberValorGorjeta50(){
     var valorGorjeta_50 = document.getElementById("gorjeta_50").value;
 }
 
+//Recebe o valor do input digitado pelo usuário//
 function receberValorGorjeta_Outro(){
     var gorjetaOutro = document.getElementById("gorjeta_outro").value;
 }
