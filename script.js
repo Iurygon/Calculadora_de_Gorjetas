25fcdf8ca0ed2dc40e473faa3191325dcb21a764
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-07-09 23:57:26 -0300

    Testes com JS

--- script.js
+++ script.js
@@ -19,35 +19,29 @@ function receberValorPessoas(){
 //Recebe o valor do input de 5%//
 function receberValorGorjeta5(){
     var valorGorjeta_5 = document.getElementById("gorjeta_5").value;
-    return valorGorjeta_5;
 }
 
 //Recebe o valor do input de 10%//
 function receberValorGorjeta10(){
     var valorGorjeta_10 = document.getElementById("gorjeta_10").value;
-    return valorGorjeta_10;
 }
 
 //Recebe o valor do input de 15%//
 function receberValorGorjeta15(){
     var valorGorjeta_15 = document.getElementById("gorjeta_15").value;
-    return valorGorjeta_15;
 }
 
 //Recebe o valor do input de 25%//
 function receberValorGorjeta25(){
     var valorGorjeta_25 = document.getElementById("gorjeta_25").value;
-    return valorGorjeta_25;
 }
 
 //Recebe o valor do input de 50%//
 function receberValorGorjeta50(){
     var valorGorjeta_50 = document.getElementById("gorjeta_50").value;
-    return valorGorjeta_50;
 }
 
 //Recebe o valor do input digitado pelo usuário//
 function receberValorGorjeta_Outro(){
     var gorjetaOutro = document.getElementById("gorjeta_outro").value;
-    return gorjetaOutro;
 }
