7dad48fcb9b140b864dadae8e5212b1fa0a844f2
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-07-10 23:25:22 -0300

    Aprimoramento do JS

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
+//Vão exibir os valores da gorjeta e conta//
 var exibeValorGorjeta = document.getElementById('tot_Gorjeta');
 var exibeValorConta = document.getElementById('tot_Conta');
-
 
 //Receber valor da conta//
 function receberValorConta(){  
     var valorConta = document.getElementById("conta").value;
-    return valorConta;
+    return parseFloat(valorConta);
 }
 
 //Receber valor do número de pessoas//
 function receberValorPessoas(){
     var numPessoas = document.getElementById("pessoas").value;
-    return numPessoas;
+    return parseFloat(numPessoas);
 }
 
 //RECEBER O VALOR DA GORJETA SELECIONADA//
 
 //Recebe o valor do input de 5%//
-function receberValorGorjeta5(){
-    var valorGorjeta_5 = document.getElementById("gorjeta_5").value;
+function receberValorGorjeta5(conta, pessoas){
+    var valorGorjeta_5 = document.getElementById("gorjeta_5").parseFLoat(value);
+    
+    valorTotalporPessoa = (conta * valorGorjeta_5) / pessoas;
+    valorGorjetaporPessoa = (conta * valorGorjeta_5 - conta) / pessoas;
+
+    exibeValorConta.innerHTML = valorTotalporPessoa;
+    exibeValorGorjeta.innerHTML = valorGorjetaporPessoa;
 }
 
 //Recebe o valor do input de 10%//
-function receberValorGorjeta10(){
+function receberValorGorjeta10(conta, pessoas){
     var valorGorjeta_10 = document.getElementById("gorjeta_10").value;
 }
 
 //Recebe o valor do input de 15%//
-function receberValorGorjeta15(){
+function receberValorGorjeta15(conta, pessoas){
     var valorGorjeta_15 = document.getElementById("gorjeta_15").value;
 }
 
 //Recebe o valor do input de 25%//
-function receberValorGorjeta25(){
+function receberValorGorjeta25(conta, pessoas){
     var valorGorjeta_25 = document.getElementById("gorjeta_25").value;
 }
 
 //Recebe o valor do input de 50%//
-function receberValorGorjeta50(){
+function receberValorGorjeta50(conta, pessoas){
     var valorGorjeta_50 = document.getElementById("gorjeta_50").value;
 }
 
 //Recebe o valor do input digitado pelo usuário//
-function receberValorGorjeta_Outro(){
+function receberValorGorjeta_Outro(conta, pessoas){
     var gorjetaOutro = document.getElementById("gorjeta_outro").value;
 }
